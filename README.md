becbbc4ee6254a220133f270f71729da555841b8
WilliamLucianooficial00 <williamlucianooficial@outlook.com.br>
2022-10-27 21:09:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Incrementar-Decrementar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rrwfiu)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public peso: number = 0;
+  public resposta: string = ' ';
+
+  exibir(){
+    console.log('Peso:' + this.peso + 'Kg');
+  }
+}
